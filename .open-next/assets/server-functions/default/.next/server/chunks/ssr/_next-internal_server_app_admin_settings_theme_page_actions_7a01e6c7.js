11f88fa1e70939d417ee5f6cb11fd671c268eb79
Aashish <aashishthakurbusiness@gmail.com>
2026-06-09 07:50:37 +0530

    chore: cleanup before filter-branch

---
+++ .open-next/assets/server-functions/default/.next/server/chunks/ssr/_next-internal_server_app_admin_settings_theme_page_actions_7a01e6c7.js
@@ -0,0 +1,3 @@
+module.exports=[72858,(a,b,c)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_admin_settings_theme_page_actions_7a01e6c7.js.map
---
+++ .open-next/assets/server-functions/default/.next/server/middleware-build-manifest.js
@@ -0,0 +1,22 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/fb8cebcab6b189f4.js",
+    "static/chunks/112f346e31f991df.js",
+    "static/chunks/6740f161f60c6ab5.js",
+    "static/chunks/a4eeb46e07741350.js",
+    "static/chunks/29141efe5227a7c5.js",
+    "static/chunks/turbopack-10ec0dcf45e0fa92.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
